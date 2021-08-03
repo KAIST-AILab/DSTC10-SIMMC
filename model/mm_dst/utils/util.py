ac3787bd56be7a45be3705c016f76953d68dcbaf
Haeju Lee <lhg912@gmail.com>
2021-08-03 15:59:37 +0900

    fixed image cropping function so that it can manage unexpected errors

--- model/mm_dst/utils/util.py
+++ model/mm_dst/utils/util.py
@@ -40,6 +40,8 @@ def given_bbox_crop_image(image_folder:str, output_folder:str, json_folder:str =
                   ...
     json_folder: if json_folder is given, this will process cropping for all the jsonfiles in that folder,
     json_file: if only json_file is given, this will process croppping only for that particular json file scene (as json filename is scene name)
+
+    Sometimes there are errors at bbox coordinates, scene.json is missing, or image is blank or not openable -> therefore rarely some crops won't be generated
     """
     assert json_folder or json_file, 'Folder_name or image_name should be given!'
     if not os.path.exists(output_folder):
@@ -52,12 +54,20 @@ def given_bbox_crop_image(image_folder:str, output_folder:str, json_folder:str =
         if scene_name:
             if not os.path.exists(os.path.join(output_folder, f'{scene_name}')):
                 os.makedirs(os.path.join(output_folder, f'{scene_name}'))
-            with open(f'{json_folder}/{scene_name}_scene.json', 'r') as f_in:
-                objs = json.load(f_in)['scenes'][0]['objects']
+            try:
+                with open(f'{json_folder}/{scene_name}_scene.json', 'r') as f_in:
+                    objs = json.load(f_in)['scenes'][0]['objects']
+            except FileNotFoundError:
+                print(f'No file named {json_folder}/{scene_name}_scene.json!!')
+                return
             image_filename = f'{scene_name[2:]}.png' if scene_name.startswith('m_') else f'{scene_name}.png'
         elif json_file:
-            with open(json_file, 'r') as f_in:
-                objs = json.load(f_in)['scenes'][0]['objects']
+            try:
+                with open(json_file, 'r') as f_in:
+                    objs = json.load(f_in)['scenes'][0]['objects']
+            except FileNotFoundError:
+                print(f'No file named {json_file}!!')
+                return
             scene_name_from_jsonfile = json_file.rsplit('_', 1)[0].rsplit('/', 1)[1]
             if not os.path.exists(os.path.join(output_folder, f'{scene_name_from_jsonfile}')):
                 os.makedirs(os.path.join(output_folder, f'{scene_name_from_jsonfile}'))
@@ -65,15 +75,22 @@ def given_bbox_crop_image(image_folder:str, output_folder:str, json_folder:str =
                 'm_') else f'{scene_name_from_jsonfile}.png'
 
         image = cv2.imread(os.path.join(image_folder, image_filename))
-        for obj in objs:
-            index = obj['index']
-            x_0, y_0, h, w = obj['bbox']
-            crop = image[y_0:y_0+h, x_0:x_0+w]
-            if scene_name:
-                cv2.imwrite('{}.png'.format(os.path.join(output_folder, scene_name, str(index))), crop)
-            elif json_file:
-                cv2.imwrite('{}.png'.format(os.path.join(
-                    output_folder, scene_name_from_jsonfile, str(index))), crop)
+        # some images are not read or blank so causing "libpng error: IDAT: CRC error"
+        if image is not None:
+            for obj in objs:
+                print(obj['index'])
+                index = obj['index']
+                x_0, y_0, h, w = obj['bbox']
+                print([y_0,y_0+h, x_0,x_0+w])
+                if h>0 and w>0:
+                    crop = image[y_0:y_0+h, x_0:x_0+w]
+                    if scene_name:
+                        cv2.imwrite('{}.png'.format(os.path.join(output_folder, scene_name, str(index))), crop)
+                    elif json_file:
+                        cv2.imwrite('{}.png'.format(os.path.join(
+                            output_folder, scene_name_from_jsonfile, str(index))), crop)
+                else:
+                    print('mislabled bbox!')
 
     if json_folder:
         for filename in os.listdir(json_folder):
@@ -86,10 +103,10 @@ def given_bbox_crop_image(image_folder:str, output_folder:str, json_folder:str =
 
 
 if __name__ == '__main__':
-    image_folder = '/Users/HeyJude/Development/GSAI/dstc/dstc10/DSTC10-SIMMC/data/images_combined'
-    output_folder = '/Users/HeyJude/Development/GSAI/dstc/dstc10/DSTC10-SIMMC/data/cropped_output'
+    image_folder = '/home/haeju/Dev/dstc/dstc10/DSTC10-SIMMC/data/images'
+    output_folder = '/home/haeju/Dev/dstc/dstc10/DSTC10-SIMMC/data/cropped_output'
     # json_file = '/Users/HeyJude/Development/GSAI/dstc/dstc10/DSTC10-SIMMC/data/jsons/m_cloth_store_1416238_woman_14_0_scene.json'
-    json_folder = '/Users/HeyJude/Development/GSAI/dstc/dstc10/DSTC10-SIMMC/data/jsons'
+    json_folder = '/home/haeju/Dev/dstc/dstc10/DSTC10-SIMMC/data/jsons'
     # given_bbox_crop_image(image_folder=image_folder, output_folder=output_folder, json_file=json_file)
     given_bbox_crop_image(image_folder=image_folder, output_folder=output_folder, json_folder=json_folder)
 
